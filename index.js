56f1e534d4c4814e3f769f40f550a79c93f24b48
Dana Kulp <dkulp23@gmail.com>
2016-09-21 16:08:28 -0700

    Added DOM script for y/n questions contained in function. Having trouble getting them to display on page.

--- index.js
+++ index.js
@@ -1,74 +1,10 @@
 'use strict';
-var userName = prompt('Hi! What\'s your name?');
-console.log(userName);
-alert('Welcome to my site, ' + userName + '. I\'d like to play a quick guessing game to help us get acquainted, ' + userName + '. Let\'s see how well you know me!');
+function greeting() {
+  var userName = prompt('Hi! What\'s your name?');
+  alert('Welcome to my site, ' + userName + '. I\'d like to play a quick guessing game to help us get acquainted, ' + userName + '. Let\'s see how well you know me!');
+}
+greeting();
 
-// var questionOne = prompt ('Was I born in Pennsylvania?', 'Yes or no...');
-// questionOne = questionOne.toLowerCase();
-// console.log(questionOne);
-// if (questionOne === 'yes' || questionOne === 'y') {
-//   alert('Correct! I was born in a suburb of Philadelphia called Lansdale.');
-// } else if (questionOne === 'no' || questionOne === 'n') {
-//   alert('Even though I don\'t have the accent I actually was born near Philly.');
-// } else {
-//   var altOne = prompt('To play the game you\'ve got to follow the rules! Yes or no please.');
-//   altOne = altOne.toLowerCase();
-//   if (altOne === 'yes' || altOne === 'y') {
-//     alert('Correct! I was born in a suburb of Philadelphia called Lansdale.');
-//   } else if (altOne === 'no' || altOne === 'n') {
-//     alert('Even though I don\'t have the accent I actually was born near Philly.');
-//   } else {
-//     var wowOne = prompt('Are you drunk?');
-//     wowOne = wowOne.toLowerCase();
-//     if (wowOne === 'yes' || wowOne === 'y') {
-//       alert('It\'s impolite not to share...');
-//     } else {
-//       alert('I\'m not sure what to say.');
-//     }
-//   }
-// }; //I can't wait to learn how to do this more elegantly!
-//
-// var questionTwo = prompt ('Is soccer my favorite sport?', 'Yes or no...');
-// questionTwo = questionTwo.toLowerCase();
-// console.log(questionTwo);
-// if (questionTwo === 'yes' || questionTwo === 'y') {
-//   alert ('Yes! There\'s a reason why they call it the beautiful game!');
-// } else if (questionTwo === 'no' || questionTwo === 'no') {
-//   alert ('I know, I probably fooled you when I talked about ping pong in my intro. That\'s a close runner up!');
-// } else {
-//   var altTwo = prompt ('Sorry, no shades of gray allowed. Yes or no only, please.');
-//   altTwo = altTwo.toLowerCase();
-//   if (altTwo === 'yes' || altTwo === 'y') {
-//     alert('You got it! I still try to play twice a week in an adult leage I\'ve been a part of since I moved to Seattle!');
-//   } else if (altTwo === 'no' || altTwo === 'n'){
-//     alert('It get\'s a lot of criticism for being boring because so few goals are scored but every second is exciting if you know what to watch for!');
-//   } else {
-//     alert('Yes or no. It\'s so simple!');
-//   }
-// };
-//
-// var questionThree = prompt ('Do I live in Fremont?', 'Yes or no...');
-// questionThree = questionThree.toLowerCase();
-// console.log(questionThree);
-// if (questionThree === 'yes' || questionThree === 'y') {
-//   alert ('Yep. Got a nice little one bedroom condo right there in the Center of the Universe.');
-// } else if (questionThree === 'no' || questionThree === 'no') {
-//   alert ('Can\'t imagine living anywhere else!');
-// } else {
-//   alert ('Still don\'t have it down by now, huh? A simple yes or no will do!');
-// };
-//
-// var questionFour = prompt ('Do I play a musical instrument?', 'Yes or no...');
-// questionFour = questionFour.toLowerCase();
-// console.log(questionFour);
-// if (questionFour === 'yes' || questionFour === 'y') {
-//   alert ('I learned how to play guitar in high school and still make noise with it now and again.');
-// } else if (questionFour === 'no' || questionFour === 'no') {
-//   alert ('OK...just because I\'m not very good at it doesn\'t mean that I don\'t \'play\' it!');
-// } else {
-//   alert ('Real slow on the uptake, eh? Please, for the love of god, just a yes or a no.');
-// };
-//
 var questions = [
   'Was I born in Pennsylvania?',
   'Is soccer my favorite sport?',
@@ -106,47 +42,36 @@ var incorrectFeedback = [
   'Nope. It is actually one of my least favorite things to do.'
 ];
 
+var responseToQuestions = [];
 var correctResponses = [];
 var incorrectResponses = [];
 
 var x = 0;
-function yesAndNo(){
-  console.log(x);
+function yesAndNo() {
   if (x < 5) {
-    var userResponse = prompt(questions[x]).toLowerCase();
-    console.log(userResponse);
+    var userResponse = prompt(questions[x], 'Please answer yes/no or y/n.').toLowerCase();
     if (userResponse === answers[x] || userResponse === abbrvAnswers[x]) {
       alert(correctFeedback[x]);
+      responseToQuestions.push(userResponse);
       correctResponses.push(userResponse);
       x++;
       yesAndNo();
     }
     else if (userResponse !== answers[x] || userResponse !== abbrvAnswers[x]){
       alert(incorrectFeedback[x]);
+      responseToQuestions.push(userResponse);
       incorrectResponses.push(userResponse);
       x++;
       yesAndNo();
-
+    } else {
+      alert('Please answer yes or no!');
+      yesAndNo();
     }
-  } else {
-    return;
   }
-
 }
 
 yesAndNo();
-// for (var x = 0; x < numberOfQuestions; x++) {
-//   var userResponse = prompt(questions[x]);
-//   var userResponseLC = userResponse.toLowerCase();
-//   if (userResponseLC === answers[x]) {
-//     console.log(userResponseLC);
-//     alert(correctFeedback[x]);
-//     correctResponses.push(userResponse);
-//   } else {
-//     alert(incorrectFeedback[x]);
-//     incorrectResponses.push(userResponse);
-//   }
-// }
+console.log(responseToQuestions);
 
 alert('Thanks for playing! You answered ' + correctResponses.length + ' out of ' + numberOfQuestions + ' questions correctly.');
 
@@ -223,21 +148,28 @@ var heyThere = hello + userName + '!';
 var hi = document.getElementById('welcome');
 hi.textContent = heyThere;
 
-var answerMessageOne = 'You answered ' + questionOne + '.';
+var answerMessageOne = 'You answered ' + responseToQuestions[0] + '.';
 var pa = document.getElementById('born');
 pa.textContent = answerMessageOne;
 
-var answerMessageTwo = 'You answered ' + questionTwo + '.';
+var answerMessageTwo = 'You answered ' + responseToQuestions[1] + '.';
 var gol = document.getElementById('sport');
 gol.textContent = answerMessageTwo;
 
-var answerMessageThree = 'You answered ' + questionThree + '.';
+var answerMessageThree = 'You answered ' + responseToQuestions[2] + '.';
 var troll = document.getElementById('live');
 troll.textContent = answerMessageThree;
 
-var answerMessageFour = 'You answered ' + questionFour + '.';
+var answerMessageFour = 'You answered ' + responseToQuestions[3] + '.';
 var hendrix = document.getElementById('musical');
 hendrix.textContent = answerMessageFour;
+
+var driveMuch = document.getElementById('doIDrive');
+driveMuch.textcontent = questions[0];
+
+var responseToQuestionFive = 'You answered ' + responseToQuestions[4] + '.';
+var armstrong = document.getElementById('iHateDriving');
+armstrong.textContent = responseToQuestionFive;
 
 var birthDay = 'You thought I was born in the ' + birthMonth + ' month of ' + birthYear + '.';
 var december = document.getElementById('sag');
